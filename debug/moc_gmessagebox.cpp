a9f2f758c94800cfe44d5cd4a37e04a410db5d6d
Unknown <guibi.ss999@gmail.com>
2018-04-26 16:12:03 -0400

    Compilation
    sous Windows 10

---
+++ debug/moc_gmessagebox.cpp
@@ -0,0 +1,118 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'gmessagebox.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../gmessagebox.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'gmessagebox.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.10.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_GMessageBox_t {
+    QByteArrayData data[4];
+    char stringdata0[32];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_GMessageBox_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_GMessageBox_t qt_meta_stringdata_GMessageBox = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "GMessageBox"
+QT_MOC_LITERAL(1, 12, 4), // "exec"
+QT_MOC_LITERAL(2, 17, 0), // ""
+QT_MOC_LITERAL(3, 18, 13) // "boutonCliquer"
+
+    },
+    "GMessageBox\0exec\0\0boutonCliquer"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_GMessageBox[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x0a /* Public */,
+       3,    0,   25,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Int,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void GMessageBox::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        GMessageBox *_t = static_cast<GMessageBox *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: { int _r = _t->exec();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 1: _t->boutonCliquer(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject GMessageBox::staticMetaObject = {
+    { &QDialog::staticMetaObject, qt_meta_stringdata_GMessageBox.data,
+      qt_meta_data_GMessageBox,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *GMessageBox::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *GMessageBox::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_GMessageBox.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int GMessageBox::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
